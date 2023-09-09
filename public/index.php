1ccca9b0c5538e017a0b00e4d8b445bcfb59a750
Ilya Shirokov <asd.mat@mail.ru>
2023-09-09 11:44:17 +0300

    update

--- public/index.php
+++ public/index.php
@@ -3,12 +3,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response,) {
-    return $response->write("Анализатор страниц");
-});
+$app->get('/', function ($request, $response) {
+    $renderer = new PhpRenderer(__DIR__ . '/../templates');
+    return $renderer->render($response, "index.phtml");
+})->setName('home');
 
 $app->run();
