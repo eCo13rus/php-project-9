7abf7c84a9a857f7d5895236aad1afa96973fb7c
Ilya Shirokov <asd.mat@mail.ru>
2023-09-10 16:09:45 +0300

    update

--- public/index.php
+++ public/index.php
@@ -1,16 +1,40 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
+use Slim\Flash\Messages;
+use DI\Container;
+use Hexlet\Code\Connection;
+use Hexlet\Code\DbRepository;
+use Valitron\Validator;
+use Carbon\Carbon;
+use DiDom\Document;
+use GuzzleHttp\Client;
 
-$app = AppFactory::create();
+session_start();
+
+$container = new Container();
+
+$container->set('renderer', function () {
+    return new PhpRenderer(__DIR__ . '/../templates');
+});
+
+$container->set('flash', function () {
+    return new Messages();
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+$router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) {
-    $renderer = new PhpRenderer(__DIR__ . '/../templates');
-    return $renderer->render($response, "index.phtml");
-})->setName('home');
+    $messages = $this->get('flash')->getMessages();
+    $params = [
+        'errors' => $messages['error'] ?? [],
+    ];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
+})->setName('main');
 
 $app->run();
