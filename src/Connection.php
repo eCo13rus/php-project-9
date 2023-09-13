67eafcf5aa236ed27b7e3f56f9a612e22c1c9e58
Ilya Shirokov <asd.mat@mail.ru>
2023-09-13 11:31:03 +0300

    update

--- src/Connection.php
+++ src/Connection.php
@@ -12,56 +12,45 @@ final class Connection
      * @return \PDO Объект PDO для взаимодействия с базой данных.
      */
     public static function connect(): \PDO
-    {
-        try {
-            // Определяем путь к директории, где находится файл .env
-            $envPath = __DIR__ . '/../';
-            
-            // Проверяем, доступен ли файл .env для чтения
-            $envFilePath = $envPath . '.env';
-            if (!is_readable($envFilePath)) {
-                throw new \RuntimeException("Файл .env не найден или недоступен для чтения в директории: $envFilePath");
-            }
-    
+{
+    try {
+        // Определяем путь к директории, где находится файл .env
+        $envPath = __DIR__ . '/../';
+        
+        // Проверяем, доступен ли файл .env для чтения
+        $envFilePath = $envPath . '.env';
+        if (is_readable($envFilePath)) {
             // Загружаем переменные окружения из файла .env
             $dotenv = \Dotenv\Dotenv::createImmutable($envPath);
             $dotenv->load();
-    
-            // Получаем текущую среду (development или production)
-            $env = getenv('APP_ENV') ?: 'production';
-    
-            // Загружаем конфигурацию базы данных
-            $dbConfig = parse_ini_file(__DIR__ . '/database.ini', true);
-    
-            // Проверяем наличие конфигурации для текущей среды
-            if (!isset($dbConfig[$env])) {
-                throw new \RuntimeException('Не найдена конфигурация БД для среды: ' . $env);
-            }
-    
-            $config = $dbConfig[$env];
-    
-            // Получаем параметры подключения
-            $username = $config['user'] ?? null;
-            $password = $config['password'] ?? null;
-            $host = $config['host'] ?? null;
-            $dbname = $config['database'] ?? null;
-    
-            // Проверяем наличие всех необходимых параметров
-            if (!$username || !$host || !$dbname) {
-                throw new \RuntimeException('Не удалось получить данные для подключения к БД');
-            }
-    
-            // Формируем строку подключения
-            $conStr = "pgsql:host=$host;dbname=$dbname";
-    
-            // Создаем объект PDO для подключения к БД
-            $pdo = new \PDO($conStr, $username, $password);
-            return $pdo;
-    
-        } catch (\Exception $e) {
-            error_log($e->getMessage());
-            throw $e;
         }
+
+        // Получаем текущую среду (development или production)
+        $env = getenv('APP_ENV') ?: 'development';
+
+        // Получаем параметры подключения из переменных окружения
+        $username = $_ENV['PGUSER'] ?? null;
+        $password = $_ENV['PGPASSWORD'] ?? null;
+        $host = $_ENV['PGHOST'] ?? null;
+        $port = $_ENV['PGPORT'] ?? null;
+        $dbname = $_ENV['PGDATABASE'] ?? null;
+       
+        // Проверяем наличие всех необходимых параметров
+        if (!$username || !$password || !$host || !$port || !$dbname) {
+            throw new \RuntimeException('Не удалось получить данные для подключения к БД');
+        }
+
+        // Формируем строку подключения
+        $conStr = "pgsql:host=$host;port=$port;dbname=$dbname";
+
+        // Создаем объект PDO для подключения к БД
+        $pdo = new \PDO($conStr, $username, $password);
+        return $pdo;
+    } catch (\Exception $e) {
+        error_log($e->getMessage());
+        throw $e;
     }
-    
+}
+
+
 }
