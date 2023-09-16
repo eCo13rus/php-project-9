c04c10b772b57312b24629897ba48067d26f8d4a
Ilya Shirokov <asd.mat@mail.ru>
2023-09-16 12:48:19 +0300

    update

--- src/Connection.php
+++ src/Connection.php
@@ -14,46 +14,49 @@ final class Connection
     public static function connect(): \PDO
     {
         try {
-            // Определяем путь к директории, где находится файл database.ini
-            $envPath = __DIR__ . '/../';
-            
-            // Загружаем параметры подключения из файла database.ini
-            $iniFilePath = $envPath . 'database.ini';
-            if (!is_readable($iniFilePath)) {
-                throw new \RuntimeException('Не удалось прочитать файл database.ini');
-            }
+            $dbConfig = self::getDbConfig();
 
-            $config = parse_ini_file($iniFilePath, true);
-            $env = $_ENV['APP_ENV'] ?? 'development';
+            $conStr = sprintf(
+                "pgsql:host=%s;port=%d;dbname=%s",
+                $dbConfig['host'],
+                $dbConfig['port'],
+                $dbConfig['database']
+            );
 
-            if (!isset($config[$env])) {
-                throw new \RuntimeException("Конфигурация для среды '{$env}' не найдена в файле database.ini");
-            }
-
-            $dbConfig = $config[$env];
-
-            $host = $dbConfig['host'] ?? null;
-            $port = $dbConfig['port'] ?? null;
-            $dbname = $dbConfig['database'] ?? null;
-            $username = $dbConfig['user'] ?? null;
-            $password = $dbConfig['password'] ?? null;
-
-            // Проверяем наличие всех необходимых параметров для подключения к БД
-            if (!$host || !$port || !$dbname || !$username || !$password) {
-                throw new \RuntimeException('Не удалось получить данные для подключения к БД');
-            }
-
-            // Формируем строку подключения
-            $conStr = "pgsql:host=$host;port=$port;dbname=$dbname";
-
-            // Создаем объект PDO для подключения к БД
-            $pdo = new \PDO($conStr, $username, $password);
+            $pdo = new \PDO($conStr, $dbConfig['user'], $dbConfig['password']);
             $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            return $pdo;
 
+            return $pdo;
         } catch (\Exception $e) {
             error_log($e->getMessage());
             throw $e;
         }
     }
+
+    private static function getDbConfig(): array
+    {
+        $iniFilePath = __DIR__ . '/../database.ini';
+        
+        $iniFilePath = realpath(__DIR__ . '/../database.ini');
+        if ($iniFilePath === false) {
+            throw new \RuntimeException('Не удалось прочитать файл database.ini');
+        }
+
+
+        $config = parse_ini_file($iniFilePath, true);
+        $env = $_ENV['APP_ENV'] ?? 'development';
+
+        if (!isset($config[$env])) {
+            throw new \RuntimeException("Конфигурация для среды '{$env}' не найдена в файле database.ini");
+        }
+
+        $dbConfig = $config[$env];
+
+        if (!$dbConfig['host'] || !$dbConfig['port'] || !$dbConfig['database'] || !$dbConfig['user'] || !$dbConfig['password']) {
+            throw new \RuntimeException('Не удалось получить данные для подключения к БД');
+        }
+
+        return $dbConfig;
+    }
+
 }
