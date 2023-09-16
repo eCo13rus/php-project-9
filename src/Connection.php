a4a1cafa27e86615759908c8fae43fa852d3a50f
Ilya Shirokov <asd.mat@mail.ru>
2023-09-16 13:10:43 +0300

    update

--- src/Connection.php
+++ src/Connection.php
@@ -12,51 +12,38 @@ final class Connection
      * @return \PDO Объект PDO для взаимодействия с базой данных.
      */
     public static function connect(): \PDO
-    {
-        try {
-            $dbConfig = self::getDbConfig();
-
-            $conStr = sprintf(
-                "pgsql:host=%s;port=%d;dbname=%s",
-                $dbConfig['host'],
-                $dbConfig['port'],
-                $dbConfig['database']
-            );
-
-            $pdo = new \PDO($conStr, $dbConfig['user'], $dbConfig['password']);
-            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
-            return $pdo;
-        } catch (\Exception $e) {
-            error_log($e->getMessage());
-            throw $e;
-        }
-    }
-
-    private static function getDbConfig(): array
-    {
-        $iniFilePath = __DIR__ . '/../database.ini';
-        
-        $iniFilePath = realpath(__DIR__ . '/../database.ini');
-        if ($iniFilePath === false) {
-            throw new \RuntimeException('Не удалось прочитать файл database.ini');
+{
+    
+    try {
+        // Подгрузка переменных окружения из .env файла для локальной среды
+        if (file_exists(__DIR__ . '/../.env')) {
+            $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+            $dotenv->load();
         }
 
+        $host = $_ENV['PGHOST'];
+        $port = $_ENV['PGPORT'];
+        $dbname = $_ENV['PGDATABASE'];
+        $username = $_ENV['PGUSER'];
+        $password = $_ENV['PGPASSWORD'];
 
-        $config = parse_ini_file($iniFilePath, true);
-        $env = $_ENV['APP_ENV'] ?? 'development';
-
-        if (!isset($config[$env])) {
-            throw new \RuntimeException("Конфигурация для среды '{$env}' не найдена в файле database.ini");
-        }
-
-        $dbConfig = $config[$env];
-
-        if (!$dbConfig['host'] || !$dbConfig['port'] || !$dbConfig['database'] || !$dbConfig['user'] || !$dbConfig['password']) {
+        // Проверка наличия всех необходимых параметров для подключения к БД
+        if (!$host || !$port || !$dbname || !$username || !$password) {
             throw new \RuntimeException('Не удалось получить данные для подключения к БД');
         }
 
-        return $dbConfig;
+        // Формирование строки подключения
+        $conStr = "pgsql:host=$host;port=$port;dbname=$dbname";
+
+        // Создание объекта PDO для подключения к БД
+        $pdo = new \PDO($conStr, $username, $password);
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+
+    } catch (\Exception $e) {
+        error_log($e->getMessage());
+        throw $e;
     }
+}
 
 }
