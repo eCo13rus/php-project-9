0af7eb4c485008ba9448f39cf5ab52ea760f6d2e
Ilya Shirokov <asd.mat@mail.ru>
2023-09-20 18:48:57 +0300

    fix

--- src/Connection.php
+++ src/Connection.php
@@ -46,6 +46,8 @@ final class Connection
             $pdo = new \PDO($dsn, $username, $password);
             $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
+            $pdo->exec("SET TIME ZONE 'Europe/Moscow';");
+
             return $pdo;
         } catch (\Exception $e) {
             error_log($e->getMessage());
